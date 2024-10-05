d7a63cb41d2130c547a700071a739f0507921029
leticiamn <leticiamedeirosnasc@gmail.com>
2024-10-05 17:27:08 -0300

    .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBgJlsUtH81O6uwW7HqhZFvlJhGZ6DDT0k",
-    authDomain: "benefit-76f8a.firebaseapp.com",
-    databaseURL: "https://benefit-76f8a-default-rtdb.firebaseio.com",
-    projectId: "benefit-76f8a",
-    storageBucket: "benefit-76f8a.appspot.com",
-    messagingSenderId: "668560513729",
-    appId: "1:668560513729:web:38b8f3cf3568b1871f5240"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Obtenha a instância de autenticação
+export const auth = getAuth(app);
 
 // Inicializa o Database
 const database = getDatabase(app);
