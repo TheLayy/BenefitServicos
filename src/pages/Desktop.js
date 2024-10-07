a6e7269de135c39b5d65841fb4e30d175dd69185
leticiamn <leticiamedeirosnasc@gmail.com>
2024-10-06 23:06:59 -0300

    Adicionados Botões Flutuantes

--- src/pages/Desktop.js
+++ src/pages/Desktop.js
@@ -11,8 +11,54 @@ import HeaderSection from "../components/HeaderSection";
 import styles from "./Desktop.module.css";
 import SmallFooter from "../components/SmallFooter";
 import NavBar from "../components/NavBar";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { Button, Fab, Fade, Box } from "@mui/material"; // Importar Fab e Fade
+import { useEffect, useState } from "react";
+
+function ScrollTop(props) {
+  const { children } = props;
+  const [trigger, setTrigger] = useState(false); // Estado para controle do trigger
+
+  const handleScroll = () => {
+    setTrigger(window.scrollY > 300); // Define o trigger quando o scroll é maior que 300
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#back-to-top-anchor"
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: "center",
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: "fixed", bottom: 85, right: 27 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
 
 const Desktop = () => {
+  const handleWhatsAppClick = () => {
+    window.open("https://wa.me/551132809870", "_blank");
+  };
 
   return (
     <div className={styles.desktop}>
@@ -29,6 +75,15 @@ const Desktop = () => {
       <Footer />
       <SmallFooter />
 
+      <ScrollTop>
+        <Fab size="small" aria-label="scroll back to top" sx={{ width: 46, height: 46 }}>
+          <ArrowUpwardIcon sx={{ fontSize: 30 }} />
+        </Fab>
+      </ScrollTop>
+
+      <button className={styles.floatingButton} onClick={handleWhatsAppClick}>
+        <img src="/whatsapp-logo.png" alt="WhatsApp" />
+      </button>
     </div>
   );
 };
