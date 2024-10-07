1298fc6fdb4b616e0370c802ed8b414411ce68e9
leticiamn <leticiamedeirosnasc@gmail.com>
2024-10-06 22:27:11 -0300

    Style página de login

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -18,25 +18,40 @@ const Auth = () => {
     return (
         <Box
             sx={{
-                maxWidth: '500px',
-                margin: 'auto',
-                padding: '20px',
-                borderRadius: '8px',
-                boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
-                backgroundColor: 'white',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100vh',
+                backgroundImage: 'url("/section2-bg.svg")',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
             }}
         >
-            <Typography variant="h5" component="div" sx={{ mb: 2 }}>
-                {currentView === 'register' ? 'Criar Conta' : currentView === 'reset' ? 'Redefinir Senha' : 'Login'}
-            </Typography>
-            {
-                currentView === 'reset' ? (
+            <Box
+                sx={{
+                    maxWidth: '500px',
+                    width: '100%',
+                    padding: '20px',
+                    borderRadius: '8px',
+                    boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+                    backgroundColor: 'white',
+                    textAlign: 'center',
+                }}
+            >
+
+                <Typography variant="h5" component="div" sx={{ mb: 2 }}>
+                    {currentView === 'register' ? 'Criar Conta' : currentView === 'reset' ? 'Redefinir Senha' : 'Login'}
+                </Typography>
+                {currentView === 'reset' ? (
                     <ResetPassword handleResetPasswordToggle={handleResetPasswordToggle} />
                 ) : currentView === 'register' ? (
                     <Register handleToggle={handleToggle} />
                 ) : (
                     <Login handleToggle={handleToggle} handleResetPasswordToggle={handleResetPasswordToggle} />
                 )}
+            </Box>
+
         </Box>
     );
 };
